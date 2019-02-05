cc1cd00c34408f70b8394efdd26ac2b1ac6eb6b8
Андрей <enengm@gmail.com>
2019-02-05 23:40:01 +0200

    2 methods for battle stage instead 1

--- src/battle/Battlefield.java
+++ src/battle/Battlefield.java
@@ -1,7 +1,6 @@
 package battle;
 
 import fighters.base.Warrior;
-import unitLabs.DragonLair;
 
 import static utilites.Helper.*;
 
@@ -12,10 +11,9 @@ public class Battlefield {
     private boolean isTournamentFinished;
     private Round pair;
     private Warrior chempion;
-   // Dragon punisher=new DragonLair().getBorned();
+    // Dragon punisher=new DragonLair().getBorned();
 
     public void startBattle() {
-
         isTournamentFinished = false;
         isRoundFinished = false;
         int stageCount = getStageCount(horde.length);
@@ -25,20 +23,22 @@ public class Battlefield {
         int round = 1;
         prepairRound(tableOfRounds, horde, round);
         // разписываем на два метода.
-        // меняем на  do while
-        while (!isTournamentFinished) {
+        round = getRound(stageCount, nextTournamentStage, winners, round);
+        tableOfRounds[round] = new Warrior[tableOfRounds[round - 1].length / 2];
+        tableOfRounds[round][0] = chempion;
+    }
+
+    private int getRound(int stageCount, Warrior[] nextTournamentStage, int winners, int round) {
+        do {
             System.out.println("round " + round + " started");
             for (int i = 0; i < tableOfRounds[round - 1].length; i += 2) {
                 pair = getNextFightersPair(tableOfRounds[round - 1], i);
-
                 pair.startRound();
-
                 if (round < stageCount - 1) nextTournamentStage[winners++] = pair.getWinner();
                 else {
                     chempion = pair.getWinner();
                 }
             }
-
             if (round == stageCount - 1) isTournamentFinished = true;
             else {
                 round++;
@@ -47,9 +47,8 @@ public class Battlefield {
                 printSummary(tableOfRounds[round - 1]);
                 nextTournamentStage = new Warrior[tableOfRounds[round - 1].length / 2];
             }
-        }
-        tableOfRounds[round] = new Warrior[tableOfRounds[round - 1].length / 2];
-        tableOfRounds[round][0] = chempion;
+        } while (!isTournamentFinished);
+        return round;
     }
 
     private int getStageCount(int length) {
@@ -63,18 +62,13 @@ public class Battlefield {
 
     public void getSummary() {
         printSummary(tableOfRounds);
-
     }
 
     public void prepairFigters() {
-        DragonLair hole = new DragonLair();
-
         horde = new Warrior[8];
-
         tableOfRounds = new Warrior[5][];
         tossUpFigters(horde);
         printSummary(horde);
-
     }
 
 //    void punish(Fighter fighter){
